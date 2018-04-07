f534b0a9fbcc88ef3025d52b9e16994a077cc5cd
Renato Soares <renatof.m.soares@gmail.com>
2018-04-07 18:47:05 -0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215000514) do
+ActiveRecord::Schema.define(version: 20180407205953) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "nome"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
